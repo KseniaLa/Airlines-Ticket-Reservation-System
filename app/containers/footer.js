be6fb1419665b3e006ccad251a58ded23d5a0360
KseniaLa <ksla1998@mail.ru>
2018-07-24 16:00:31 +0300

    updates

--- app/containers/footer.js
+++ app/containers/footer.js
@@ -3,18 +3,18 @@ import Menu from './basic/menu';
 import SocialIcon from './basic/socialicon';
 
 export default class Footer extends React.Component {
-    render() {
-      return (
-        <footer className='container-flex footer'>
-          <div className='content-flex footer__content'>
-            <Menu items={['О компании', 'Реклама', 'Вакансии', 'Помощь']} />
-            <div className='footer__social'>
-              <SocialIcon icon='fa fa-vk' />
-              <SocialIcon icon='fa fa-facebook-f' />
-              <SocialIcon icon='fa fa-twitter' />
-            </div>
+  render() {
+    return (
+      <footer className='container-flex footer'>
+        <div className='content-flex footer__content'>
+          <Menu items={['О компании', 'Реклама', 'Вакансии', 'Помощь']} />
+          <div className='footer__social'>
+            <SocialIcon icon='fa fa-vk' />
+            <SocialIcon icon='fa fa-facebook-f' />
+            <SocialIcon icon='fa fa-twitter' />
           </div>
-        </footer>
-      )
-    }
+        </div>
+      </footer>
+    )
   }
+}

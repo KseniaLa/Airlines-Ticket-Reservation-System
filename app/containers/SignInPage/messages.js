daba2f27d85e12090635e7772f3476487d7b5e77
Ksenia Lashchinovskaya <ksLa1998@mail.ru>
2018-07-30 15:37:46 +0300

    sign in page messages started

---
+++ app/containers/SignInPage/messages.js
@@ -0,0 +1,20 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  signin: {
+    id: 'app.components.SignInPage.signin',
+    defaultMessage: 'Войти',
+  },
+  signup: {
+    id: 'app.components.SignInPage.signup',
+    defaultMessage: 'Зарегистрироваться',
+  },
+  signuptitle: {
+    id: 'app.components.SignInPage.signuptitle',
+    defaultMessage: 'Регистрация',
+  },
+  signintitle: {
+    id: 'app.components.SignInPage.signintitle',
+    defaultMessage: 'Авторизация',
+  },
+});

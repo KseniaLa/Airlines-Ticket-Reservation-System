7e00a0fbd1b8407354da61a7156287cbe38d22d8
KseniaLa <ksla1998@mail.ru>
2018-08-08 23:16:11 +0300

    start authorization

--- AirlinesTecketsReservationApp/AirlinesTecketsReservationApp/Startup.cs
+++ AirlinesTecketsReservationApp/AirlinesTecketsReservationApp/Startup.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using AirlinesTicketsReservationApp.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Models;
 
 namespace AirlinesTecketsReservationApp
@@ -28,6 +31,22 @@ namespace AirlinesTecketsReservationApp
         public void ConfigureServices(IServiceCollection services)
         {
                services.AddCors();
+
+               services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+              .AddJwtBearer(jwtBearerOptions =>
+              {
+                   jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters()
+                   {
+                        ValidateActor = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["Issuer"],
+                        ValidAudience = Configuration["Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["SigningKey"]))
+                   };
+              });
+
                services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
                services.AddTransient<IAirlinesContext, AirlinesContext>();
                services.AddDbContext<AirlinesContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));

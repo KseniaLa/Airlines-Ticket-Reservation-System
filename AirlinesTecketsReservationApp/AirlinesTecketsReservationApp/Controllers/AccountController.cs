6522f5b0dee95cee7d85f4367276aab67315b0ab
Ksenia Lashchinovskaya <ksLa1998@mail.ru>
2018-08-27 09:17:20 +0300

    update token validation

--- AirlinesTecketsReservationApp/AirlinesTecketsReservationApp/Controllers/AccountController.cs
+++ AirlinesTecketsReservationApp/AirlinesTecketsReservationApp/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Net.Mail;
 using System.Security.Claims;
@@ -13,88 +14,100 @@ using Microsoft.AspNetCore.Http;
 using AirlinesApp.DataPresentation;
 using AirlinesApp.Exceptions;
 using AutoMapper;
+using Microsoft.Extensions.Primitives;
 
 namespace AirlinesTicketsReservationApp.Controllers
 {
-     [Route("api/account")]
-     public class AccountController : Controller
-     {
-          private readonly IAccountService _accountService;
-          private readonly IIpService _ipService;
-          private readonly IHttpContextAccessor _accessor;
-          private readonly ITokenManager _tokenManager;
+    [Route("api/account")]
+    public class AccountController : Controller
+    {
+        private readonly IAccountService _accountService;
+        private readonly IIpService _ipService;
+        private readonly IHttpContextAccessor _accessor;
+        private readonly ITokenManager _tokenManager;
 
-          public AccountController(IHttpContextAccessor accessor, IAccountService accountService, IIpService ipService, ITokenManager tokenManager)
-          {
-               _accountService = accountService;
-               _ipService = ipService;
-               _accessor = accessor;
-               _tokenManager = tokenManager;
-          }
+        public AccountController(IHttpContextAccessor accessor, IAccountService accountService, IIpService ipService, ITokenManager tokenManager)
+        {
+            _accountService = accountService;
+            _ipService = ipService;
+            _accessor = accessor;
+            _tokenManager = tokenManager;
+        }
 
-          [AllowAnonymous]
-          [HttpPost("login")]
-          public async Task<IActionResult> Authenticate([FromBody]LoginModel user)
-          {
-               User usr = await _accountService.TryAuthenticate(user.Email, user.Password);
-               if (usr != null)
-               {
-                    var ip = _accessor.HttpContext.Connection.RemoteIpAddress.ToString();
-                    await _ipService.AddUserIpAddress(usr.Id, ip);
-                    string token = JwtGenerator.GenerateToken(usr);
-                    return Ok(new { token, name = usr.Name, surname = usr.Surname, isAdmin = usr.IsAdmin });
-               }
-               return BadRequest();
-          }
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<IActionResult> Authenticate([FromBody]LoginModel user)
+        {
+            User usr = await _accountService.TryAuthenticate(user.Email, user.Password);
+            if (usr != null)
+            {
+                var ip = _accessor.HttpContext.Connection.RemoteIpAddress.ToString();
+                await _ipService.AddUserIpAddress(usr.Id, ip);
+                string token = JwtGenerator.GenerateToken(usr);
+                return Ok(new { token, name = usr.Name, surname = usr.Surname, isAdmin = usr.IsAdmin });
+            }
+            return BadRequest();
+        }
 
-          [Authorize]
-          [HttpPost("update")]
-          public async Task<IActionResult> UpdateUser()
-          {
-               if (!await _tokenManager.IsCurrentActiveToken())
-               {
-                    throw new UnauthorizedException();
-               }
-               string email = HttpContext.User.FindFirst(ClaimTypes.Email).Value;
-               User usr = await _accountService.GetUserByEmail(email);
-               if (usr != null)
-               {
-                    string token = JwtGenerator.GenerateToken(usr);
-                    return Ok(new { token, name = usr.Name, surname = usr.Surname, isAdmin = usr.IsAdmin });
-               }
-               return BadRequest();
-          }
+        [Authorize]
+        [HttpPost("update")]
+        public async Task<IActionResult> UpdateUser()
+        {
+            //if (!await _tokenManager.IsCurrentActiveToken())
+            //{
+            //     throw new UnauthorizedException();
+            //}
+            string email = HttpContext.User.FindFirst(ClaimTypes.Email).Value;
+            var authorizationHeader = HttpContext.Request.Headers["authorization"];
 
-          [AllowAnonymous]
-          [HttpPost("signup")]
-          public async Task<IActionResult> Register([FromBody]SignupModel user)
-          {
-               await EmailService.SendTestEmail(user.Email);
-               User usr = await _accountService.SignUp(user);
-               if (usr != null)
-               {
-                    return Ok();
-               }
-               return BadRequest();
-          }
+            string requestToken = authorizationHeader == StringValues.Empty
+                ? string.Empty
+                : authorizationHeader.Single().Split(" ").Last();
+            User usr = await _accountService.GetUserByEmail(email);
+            if (usr != null)
+            {
+                string role = HttpContext.User.FindFirst(ClaimTypes.Role).Value;
+                string currRole = usr.IsAdmin ? Roles.Administrator : Roles.User;
+                if (currRole == role)
+                {
+                    return Ok(new { token = requestToken, name = usr.Name, surname = usr.Surname, isAdmin = usr.IsAdmin });
+                }
+                string token = JwtGenerator.GenerateToken(usr);
+                return Ok(new { token, name = usr.Name, surname = usr.Surname, isAdmin = usr.IsAdmin });
+            }
+            return BadRequest();
+        }
 
-          [Authorize]
-          [HttpGet("iphistory")]
-          public async Task<IActionResult> GetUserIpHistory()
-          {
-               string email = HttpContext.User.FindFirst(ClaimTypes.Email).Value;
-               User user = await _accountService.GetUserByEmail(email);
-               List<IpAddressModel> addresses = await _ipService.GetUserIpAddressLatest(user.Id, 10);
-               return Ok(new { addresses });
-          }
+        [AllowAnonymous]
+        [HttpPost("signup")]
+        public async Task<IActionResult> Register([FromBody]SignupModel user)
+        {
+            await EmailService.SendTestEmail(user.Email);
+            User usr = await _accountService.SignUp(user);
+            if (usr != null)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
 
-          
-          [HttpPost("tokens/cancel")]
-          public async Task<IActionResult> CancelAccessToken()
-          {
-               await _tokenManager.DeactivateCurrentAsync();
+        [Authorize]
+        [HttpGet("iphistory")]
+        public async Task<IActionResult> GetUserIpHistory()
+        {
+            string email = HttpContext.User.FindFirst(ClaimTypes.Email).Value;
+            User user = await _accountService.GetUserByEmail(email);
+            List<IpAddressModel> addresses = await _ipService.GetUserIpAddressLatest(user.Id, 10);
+            return Ok(new { addresses });
+        }
 
-               return NoContent();
-          }
-     }
+
+        [HttpPost("tokens/cancel")]
+        public async Task<IActionResult> CancelAccessToken()
+        {
+            await _tokenManager.DeactivateCurrentAsync();
+
+            return NoContent();
+        }
+    }
 }
